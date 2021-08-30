def651705ab2ffb419b4dc38e7080e09b61ef6d4
Tomislav Jakopec <tomislav@jakopec.com>
2021-08-30 18:49:32 +0200

    Prvo startanje Hibernate

--- APP/src/main/java/edunova/jp24/Start.java
+++ APP/src/main/java/edunova/jp24/Start.java
@@ -5,10 +5,16 @@
  */
 package edunova.jp24;
 
+import edunova.jp24.util.HibernateUtil;
+
 /**
  *
  * @author dell
  */
 public class Start {
+    
+    public static void main(String[] args) {
+        HibernateUtil.getSession();
+    }
     
 }
