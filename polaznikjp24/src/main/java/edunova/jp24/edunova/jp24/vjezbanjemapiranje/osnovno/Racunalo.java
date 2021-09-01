56e46a8415145dc3ff252490943e6e9714d0e001
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-01 17:19:28 +0100

    racunalo

---
+++ polaznikjp24/src/main/java/edunova/jp24/edunova/jp24/vjezbanjemapiranje/osnovno/Racunalo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.edunova.jp24.vjezbanjemapiranje.osnovno;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jalep
+ */
+@Entity
+public class Racunalo {
+    
+    @Id
+    private int sifra;
+    private String naziv;
+    private BigDecimal cijena;
+
+    public int getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(int sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public BigDecimal getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(BigDecimal cijena) {
+        this.cijena = cijena;
+    }
+    
+    
+    
+}
