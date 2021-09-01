fdc8092446361cdc5a46d5f9c792295d42d91024
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-01 18:01:59 +0100

    ucionicaIVozilo

---
+++ polaznikjp24/src/main/java/edunova/jp24/vjezbanjemapiranje/osnovno/prilagodeno/Ucionica.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.vjezbanjemapiranje.osnovno.prilagodeno;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jalep
+ */
+@Entity(name = "class_room")
+public class Ucionica {
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sifra;
+    
+    @Column (name = "classroom",
+            columnDefinition = "text")
+    
+    private int polaznic;
+    private int predavaci;
+    
+    
+    // Definirajte 3 svojstva, svkom dodatno definirajte znacajke. Tablica neka se zove
+    // classrooom
+
+    public int getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(int sifra) {
+        this.sifra = sifra;
+    }
+
+    public int getPolaznic() {
+        return polaznic;
+    }
+
+    public void setPolaznic(int polaznic) {
+        this.polaznic = polaznic;
+    }
+
+    public int getPredavaci() {
+        return predavaci;
+    }
+
+    public void setPredavaci(int predavaci) {
+        this.predavaci = predavaci;
+    }
+}
