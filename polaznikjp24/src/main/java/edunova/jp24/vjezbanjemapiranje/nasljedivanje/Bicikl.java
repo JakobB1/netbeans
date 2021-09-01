b33977c0d4ce035e6d597ffcf9fe44b968703495
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-01 18:06:24 +0100

    bicikl

---
+++ polaznikjp24/src/main/java/edunova/jp24/vjezbanjemapiranje/nasljedivanje/Bicikl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.vjezbanjemapiranje.nasljedivanje;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jalep
+ */
+
+@Entity(name = "bike")
+public class Bicikl extends Vozilo{
+    
+    @Column(length = 11, columnDefinition = "char")
+    private String vozacOiB;
+
+    public String getVozacOiB() {
+        return vozacOiB;
+    }
+
+    public void setVozacOiB(String vozacOiB) {
+        this.vozacOiB = vozacOiB;
+    }
+    
+    
+}
