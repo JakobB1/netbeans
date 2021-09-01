f4005fa80d7483e113c9d1b90a53ac000ce4122c
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-01 17:27:21 +0100

    olovka

---
+++ polaznikjp24/src/main/java/edunova/jp24/edunova/jp24/vjezbanjemapiranje/osnovno/Olovka.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.edunova.jp24.vjezbanjemapiranje.osnovno;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jalep
+ */
+@Entity
+public class Olovka {
+    
+    @Id
+    private int sifra;
+    private String naziv;
+    private String oznaka;
+    
+    // opisite olovku s 5 svojstava, mapirajte na bazu i izvedite program
+    // u bazi mora biti tablica Olovka
+
+    public int getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(int sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka = oznaka;
+    }
+}
