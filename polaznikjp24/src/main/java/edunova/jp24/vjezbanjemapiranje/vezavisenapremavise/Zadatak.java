cc1154193acc0d484206948bd78926e51a51952e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-01 18:46:28 +0100

    zadatak

---
+++ polaznikjp24/src/main/java/edunova/jp24/vjezbanjemapiranje/vezavisenapremavise/Zadatak.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.vjezbanjemapiranje.vezavisenapremavise;
+
+/**
+ *
+ * @author jalep
+ */
+public class Zadatak {
+    
+    // Iz ERA dijagrama svog zavrsnog rada kreirajte dvije klase koje su ekvivalent 
+    // dvijema tablicama povezanim vezom 1:n
+    
+    // Ako netko nema u svojoj bazi n:n neka smisli i realizira jedan primjer
+}
