158d78c87f18479f43a482e423c9585527d6f456
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-06 11:18:26 +0100

    oznakaSlika

---
+++ polaznikjp24/src/main/java/edunova/jp24/vjezbanjemapiranje/vezavisenapramavise/Slika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.vjezbanjemapiranje.vezavisenapramavise;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jalep
+ */
+@Entity
+public class Slika {
+    
+    @Id
+    private Long id;
+    
+    private String naziv;
+    
+    private List<Oznaka> oznake;
+}
