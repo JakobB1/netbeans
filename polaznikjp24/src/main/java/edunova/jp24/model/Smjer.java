175ff5f480e53248830760a3e2ceda8e96796d36
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-06 17:19:05 +0100

    smjer

---
+++ polaznikjp24/src/main/java/edunova/jp24/model/Smjer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.model;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jalep
+ */
+@Entity
+public class Smjer extends Entitet{
+    
+    private String naziv;
+    private Integer trajanje;
+    private BigDecimal cijena;
+    private Boolean certifikat;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public Integer getTrajanje() {
+        return trajanje;
+    }
+
+    public void setTrajanje(Integer trajanje) {
+        this.trajanje = trajanje;
+    }
+
+    public BigDecimal getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(BigDecimal cijena) {
+        this.cijena = cijena;
+    }
+
+    public Boolean getCertifikat() {
+        return certifikat;
+    }
+
+    public void setCertifikat(Boolean certifikat) {
+        this.certifikat = certifikat;
+    }
+    
+}
