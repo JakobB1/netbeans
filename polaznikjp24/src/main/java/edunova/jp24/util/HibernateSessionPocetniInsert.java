477a784dd068b9b8c9993482e8178591e0afa6e4
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-09 17:50:48 +0100

    session

--- polaznikjp24/src/main/java/edunova/jp24/util/HibernateSessionPocetniInsert.java
+++ polaznikjp24/src/main/java/edunova/jp24/util/HibernateSessionPocetniInsert.java
@@ -5,18 +5,10 @@
  */
 package edunova.jp24.util;
 
-import com.github.javafaker.Faker;
-import edunova.jp24.model.Polaznik;
-import edunova.jp24.model.Predavac;
 import edunova.jp24.model.Smjer;
-import java.util.ArrayList;
-import java.util.List;
 import org.hibernate.Session;
 
-/**
- *
- * @author jalep
- */
+
 public class HibernateSessionPocetniInsert {
     
     public static void odradiPosao() {
@@ -25,43 +17,11 @@ public class HibernateSessionPocetniInsert {
         // prije rada s hibernate zapoceti transakciju
         s.beginTransaction();
         
-        
-        Smjer java = new Smjer();
-        java.setNaziv("Java programiranje");
-        s.save(java);
-        
-        Faker faker = new Faker();
-        Predavac predavac  = new Predavac();
-        predavac.setIme(faker.name().firstName());
-        predavac.setPrezime(faker.name().lastName());
-        
-        s.save(predavac);
-        
-        
-        List<Polaznik> polaznici = new ArrayList<>();
-        
-        /*
-        Polaznik polaznik;
-        for (int i = 0; i < 1300; i++) {
-            polaznik = new Polaznik();
-            polaznik.setIme(faker.name().firstName());
-            polaznik.setPrezime(faker.name().lastName());
-            s.save(polaznik);
-            
-            if(i%100==0)
-                polaznici.add(polaznik);
-        }
-        
-        
-        
         for(int i = 1; i<=100; i++) {
             Smjer smjer = new Smjer();
             smjer.getNaziv("Smjer" + i);
             s.save(smjer);
-        }*/
-        
-        
-                
+        }     
         // commit ide na kraju        
         s.getTransaction().commit();
     }
