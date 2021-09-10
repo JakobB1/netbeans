341a3a0a335a96945479ac2ae1ba2e3f8e871d55
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-10 17:15:16 +0100

    obradaPOlaznik

--- polaznikjp24/src/main/java/edunova/jp24/contoller/ObradaPolaznik.java
+++ polaznikjp24/src/main/java/edunova/jp24/contoller/ObradaPolaznik.java
@@ -5,10 +5,34 @@
  */
 package edunova.jp24.contoller;
 
+import edunova.jp24.model.Polaznik;
+import edunova.jp24.util.EdunovaException;
+import java.util.List;
+
 /**
  *
  * @author jalep
  */
-public class ObradaPolaznik {
+public class ObradaPolaznik extends Obrada<Polaznik>{
+
+    @Override
+    public List<Polaznik> read() {
+        return session.createQuery("from Polaznik").list();
+    }
+
+    @Override
+    protected void kontrolaCreate() throws EdunovaException {
+    
+    }
+
+    @Override
+    protected void kontrolaUpdate() throws EdunovaException {
+        
+    }
+
+    @Override
+    protected void kontrolaDelete() throws EdunovaException {
+        
+    }
     
 }
