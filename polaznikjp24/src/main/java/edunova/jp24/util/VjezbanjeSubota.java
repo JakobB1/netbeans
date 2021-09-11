8ed6d4e8d0027c16511949aef7587f2625085c50
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-11 09:42:28 +0100

    vjezba

---
+++ polaznikjp24/src/main/java/edunova/jp24/util/VjezbanjeSubota.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.util;
+
+/**
+ *
+ * @author jalep
+ */
+public class VjezbanjeSubota {
+    
+    public static void main(String[] args) {
+        // Zbrojiti prvih 67 brojeva i ispisati rez
+        int sum = 0;
+        for (int i=0; i<=67; i++)
+            
+            sum = sum+i;
+
+        
+    }   
+}
+
