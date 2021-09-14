cde43369aa14a39670011fc1e488b550e7f880ba
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-14 17:05:04 +0100

    zadaci

---
+++ PcShopMVC/src/main/java/hr/edunova/pcshopmvc/vjezbanjemapiranje/nasljedivanje/Zadatak.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.pcshopmvc.vjezbanjemapiranje.nasljedivanje;
+
+/**
+ *
+ * @author jalep
+ */
+public class Zadatak {
+    
+    //Osmisliti nasljeđivanje na tri razine i mapirati ga na bazu
+    
+}
---
+++ PcShopMVC/src/main/java/hr/edunova/pcshopmvc/vjezbanjemapiranje/vezavisenapremajedan/Zadatak.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.pcshopmvc.vjezbanjemapiranje.vezavisenapremajedan;
+
+/**
+ *
+ * @author jalep
+ */
+public class Zadatak {
+    
+     // Iz ERA dijagrama svoj završnog rada kreirajte dvije klase koje su ekvivalent
+    // dvijema tablicama povezanim vezom 1:n
+    
+}
