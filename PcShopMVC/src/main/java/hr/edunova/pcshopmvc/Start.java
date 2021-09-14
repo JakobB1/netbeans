37d8b3c68150dc97161a19cce40e95a64738543d
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-14 15:00:01 +0100

    rename

--- PcShopMVC/src/main/java/hr/edunova/pcshopmvc/Start.java
+++ PcShopMVC/src/main/java/hr/edunova/pcshopmvc/Start.java
@@ -14,8 +14,6 @@ import org.hibernate.Session;
  */
 public class Start {
     public static void main(String[] args) {
-        Session s = HibernateUtil.getSession();
-        
-        
+        HibernateUtil.getSession();
     }
 }
