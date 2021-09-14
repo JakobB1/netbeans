bd75e90944ee63dfe6502693e795743a70870449
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-14 17:23:15 +0100

    zadatak

---
+++ PcShopMVC/src/main/java/hr/edunova/pcshopmvc/vjezbanjemapiranje/vezavisenapremavise/Zadatak.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.pcshopmvc.vjezbanjemapiranje.vezavisenapremavise;
+
+/**
+ *
+ * @author jalep
+ */
+public class Zadatak {
+    
+    // Iz ERA dijagrama svoj završnog rada kreirajte dvije klase koje su ekvivalent
+    // dvijema tablicama povezanim vezom n:n.
+    
+    // Ako netko nema u svojoj bazi n:n onda neka smisli i realizira jedan primjer
+    
+}
