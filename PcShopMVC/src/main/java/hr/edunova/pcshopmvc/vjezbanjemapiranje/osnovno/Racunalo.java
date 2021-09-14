bc734fe454b694cd7c39f118c647a2d2e691cd35
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-14 14:41:41 +0100

    racunalo
    getters and setters

---
+++ PcShopMVC/src/main/java/hr/edunova/pcshopmvc/vjezbanjemapiranje/osnovno/Racunalo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.pcshopmvc.vjezbanjemapiranje.osnovno;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author jalep
+ */
+public class Racunalo {
+    
+    private int sifra;
+    private String naziv;
+    private BigDecimal cijena;
+
+    public int getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(int sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public BigDecimal getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(BigDecimal cijena) {
+        this.cijena = cijena;
+    }
+    
+    
+}
