1ef57f1a10ce4a5a2ea425d0b8b9fcc198acdd88
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-14 13:25:56 +0100

    delete

--- polaznikjp24/src/main/java/edunova/jp24/contoller/ObradaSmjer.java
+++ polaznikjp24/src/main/java/edunova/jp24/contoller/ObradaSmjer.java
@@ -19,68 +19,60 @@ public class ObradaSmjer extends Obrada<Smjer>{
 
     @Override
     public List<Smjer> read() {
-        return session.createQuery("from Smjer").list();
+           return session.createQuery("from Smjer").list();
     }
 
     @Override
-    protected void kontrolaCreate() throws EdunovaException{
+    protected void kontrolaCreate() throws EdunovaException {
         kontrolaNaziv();
         kontrolaCertifikat();
     }
 
     @Override
-    protected void kontrolaUpdate() throws EdunovaException{
-        kontrolaNaziv();
+    protected void kontrolaUpdate() throws EdunovaException {
+         kontrolaNaziv();
     }
 
     @Override
-    protected void kontrolaDelete() throws EdunovaException{
-        
+    protected void kontrolaDelete() throws EdunovaException {
+   
     }
 
     private void kontrolaNaziv() throws EdunovaException {
-        
+       
         if(entitet.getNaziv()==null || entitet.getNaziv().trim().length()==0){
-            throw new EdunovaException("Naziv obavezno");
-        }
+           throw new EdunovaException("Naziv obavezno");
+       }
         
         if(entitet.getNaziv().length()>50){
-            throw new EdunovaException("Naziv ne moze biti duzi od 50 znakova");
-        }        
-        // Naziv u sebi ne smije sadrzavati brojeve
-
+            throw new EdunovaException("Naziv ne može biti duži od 50 znakova");
+        }
+        
         /*
         List<Smjer> sviSmjerovi = read();
         for(Smjer s: sviSmjerovi){
             if(s.getNaziv().equals(entitet.getNaziv())){
-                throw new EdunovaException("Naziv vec postoji");
+                throw new EdunovaException("Naziv već postoji");
             }
         }
         */
-
-        /*Query q = session.createNamedQuery("select count(*) from smjer where naziv =:nazivParametar );
-        q.setParameter("nazivParametar", entitet.getNaziv());
         
-        BigInteger ukupno = (BigInteger)q.setSingleResult();
-                
-        if(ukupno.compareTo(BigInteger.ZERO).0){
-            throw new EdunovaException("Naziv vec postoji");
-        }*/
+        
+        Query q = session.createNativeQuery("select count(*) from smjer where naziv=:nazivParametar");
+        q.setParameter("nazivParametar", entitet.getNaziv());
+      
+        BigInteger ukupno = (BigInteger)q.getSingleResult();
+     
+        if(ukupno.compareTo(BigInteger.ZERO)>0){
+             throw new EdunovaException("Naziv već postoji");
+        }
+        
     }
 
     private void kontrolaCertifikat() throws EdunovaException{
-        if(entitet.getCertifikat()==null){
-            throw new EdunovaException("Indikacija certificiranosti smjera obavezna");
-        }
-    }
-    
-    private void kontrolaImaLiBroj() throws EdunovaException{
-        
-        for(char c : entitet.getNaziv().toCharArray()){
-            if(Character.isDigit(c)){
-                throw new EdunovaException("Naziv ne smije zadrzavati broj");
-            }
-        }
+      if(entitet.getCertifikat()==null){
+          throw new EdunovaException("Indikacija certificiranosti smjera obavezna");
+      }  
     }
     
 }
