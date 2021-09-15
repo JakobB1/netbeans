62defb1fe6fa97301c98386f204b46e1beb8f5ab
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-15 11:35:31 +0100

    obradaPredavac

---
+++ polaznikjp24/src/main/java/edunova/jp24/controller/ObradaPredavac.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.controller;
+
+import edunova.jp24.model.Predavac;
+import edunova.jp24.util.EdunovaException;
+import java.util.List;
+
+/**
+ *
+ * @author jalep
+ */
+public class ObradaPredavac extends Obrada<Predavac>{
+
+    @Override
+    public List<Predavac> read() {
+        return session.createQuery("from Polaznik").list();
+    }
+
+    @Override
+    protected void kontrolaCreate() throws EdunovaException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void kontrolaUpdate() throws EdunovaException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void kontrolaDelete() throws EdunovaException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
