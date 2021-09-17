3415438bc566f62e7471707377ddcb7630473748
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-17 16:54:11 +0100

    operater

---
+++ polaznikjp24/src/main/java/edunova/jp24/model/Operater.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jalep
+ */
+@Entity
+public class Operater extends Osoba{
+    
+    private String lozinka;
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+    
+    
+}
