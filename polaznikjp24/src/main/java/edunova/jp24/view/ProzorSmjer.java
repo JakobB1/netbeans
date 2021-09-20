07407666b717327e641aa724528141d5ac8df4de
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-20 18:32:57 +0100

    prozor

--- polaznikjp24/src/main/java/edunova/jp24/view/ProzorSmjer.java
+++ polaznikjp24/src/main/java/edunova/jp24/view/ProzorSmjer.java
@@ -8,7 +8,12 @@ package edunova.jp24.view;
 import edunova.jp24.controller.ObradaSmjer;
 import edunova.jp24.model.Smjer;
 import edunova.jp24.util.Aplikacija;
+import edunova.jp24.util.EdunovaException;
+import java.math.BigDecimal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -51,10 +56,38 @@ public class ProzorSmjer extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         lstSmjerovi = new javax.swing.JList<>();
+        jLabel1 = new javax.swing.JLabel();
+        txtNaziv = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        txtTrajanje = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        txtCijena = new javax.swing.JTextField();
+        chbCertifikat = new javax.swing.JCheckBox();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
+        lstSmjerovi.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                lstSmjeroviValueChanged(evt);
+            }
+        });
         jScrollPane1.setViewportView(lstSmjerovi);
+
+        jLabel1.setText("Naziv");
+
+        jLabel2.setText("Trajanje");
+
+        jLabel3.setText("Cijena");
+
+        chbCertifikat.setText("Certificirano");
+
+        jButton1.setText("Dodaj");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -63,24 +96,102 @@ public class ProzorSmjer extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(250, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(txtNaziv, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtTrajanje, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtCijena, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(chbCertifikat)
+                    .addComponent(jButton1))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(39, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtNaziv, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtTrajanje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtCijena, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(chbCertifikat)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1)))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        obradaSmjer.setEntitet(new Smjer());
+        var s = obradaSmjer.getEntitet();
+        s.setNaziv(txtNaziv.getText());
+        try {
+            s.setTrajanje(Integer.parseInt(txtTrajanje.getText()));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(getParent(), "Trajanje mora biti cijeli broj");
+            return;
+        }
+        try {
+            s.setCijena(new BigDecimal(txtCijena.getText()));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(getParent(), "Cijena mora biti decimalni broj");
+            return;
+        }
+        s.setCertifikat(chbCertifikat.isSelected());
+        try {
+            obradaSmjer.create();
+            ucitajSmjerove();
+        } catch (EdunovaException ex) {
+            JOptionPane.showMessageDialog(getParent(), ex.getPoruka());
+        }
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void lstSmjeroviValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_lstSmjeroviValueChanged
+         if(evt.getValueIsAdjusting() || lstSmjerovi.getSelectedValue()==null){
+             return;
+         }
+         obradaSmjer.setEntitet(lstSmjerovi.getSelectedValue());
+         var s = obradaSmjer.getEntitet();
+         txtNaziv.setText(s.getNaziv());
+         txtTrajanje.setText(String.valueOf(s.getTrajanje()));
+         txtCijena.setText(String.valueOf(s.getCijena()));
+         chbCertifikat.setSelected(s.getCertifikat());
+         
+         
+         
+    }//GEN-LAST:event_lstSmjeroviValueChanged
+
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox chbCertifikat;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JList<Smjer> lstSmjerovi;
+    private javax.swing.JTextField txtCijena;
+    private javax.swing.JTextField txtNaziv;
+    private javax.swing.JTextField txtTrajanje;
     // End of variables declaration//GEN-END:variables
 
     
