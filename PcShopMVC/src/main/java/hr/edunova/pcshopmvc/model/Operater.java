54dc25c8f996f21b23871c9c0c76e7bca799204c
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-21 17:59:04 +0100

    operater

---
+++ PcShopMVC/src/main/java/hr/edunova/pcshopmvc/model/Operater.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.pcshopmvc.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jalep
+ */
+@Entity
+public class Operater extends Entitet{
+    
+    private String lozinka;
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+    
+    
+}
