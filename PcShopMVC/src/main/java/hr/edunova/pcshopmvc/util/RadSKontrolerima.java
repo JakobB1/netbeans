cb0ecd8cd2d3025a2e012ee71ce08fb766a28035
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-09-21 10:06:06 +0100

    radSKontr

---
+++ PcShopMVC/src/main/java/hr/edunova/pcshopmvc/util/RadSKontrolerima.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.pcshopmvc.util;
+
+import hr.edunova.pcshopmvc.contoller.ObradaPcshop;
+import hr.edunova.pcshopmvc.model.Pcshop;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jalep
+ */
+public class RadSKontrolerima {
+    
+    public static void kreirajPcshop(){
+        ObradaPcshop os = new ObradaPcshop();
+        Pcshop p = new Pcshop();
+        os.setEntitet(p);
+        
+        try {
+            os.create();
+        } catch (EdunovaException ex) {
+            System.out.println(ex.getPoruka());
+        }
+        
+        
+    }
+    
+}
