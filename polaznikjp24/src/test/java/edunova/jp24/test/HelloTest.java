f1d008bce5cae3e5f75089c29eb5b8c690f93f65
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-10-02 09:59:08 +0100

    vjezbanjeSub(2.10.)

---
+++ polaznikjp24/src/test/java/edunova/jp24/test/HelloTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.test;
+
+
+import edunova.jp24.model.Smjer;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author dell
+ */
+public class HelloTest {
+    
+    // koraci: google traženje koje me krivo odvelo
+    // nakon čitanja shvatili ono što je dobro:
+    // https://javarevisited.blogspot.com/2013/03/how-to-write-unit-test-in-java-eclipse-netbeans-example-run.html#axzz787UyKWIR
+    // https://examples.javacodegeeks.com/core-java/junit/junit-netbeans-example/
+    
+    //šire čitanje
+    //https://www.toptal.com/java/getting-started-with-junit
+    // https://www.partech.nl/nl/publicaties/2020/03/10-unit-testing-best-practices#
+    
+    
+    private Smjer smjer;
+ 
+    public HelloTest() {
+        smjer= new Smjer();
+        smjer.setNaziv("Programiranje");
+    }
+
+    @Test
+    public void nazivSmjera() {
+        assertEquals("Naziv smjera nije dobar","Programiranje",smjer.getNaziv());
+        
+    }
+    @Test
+    public void duzinaSmjera(){
+        assertTrue("Dužina mora biti manja od 20 znakova", smjer.getNaziv().length()<20);
+    }
+    
+    
+    
+}
---
+++ polaznikjp24/src/test/java/edunova/jp24/test/TestObradaSmjer.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.jp24.test;
+
+import edunova.jp24.controller.ObradaSmjer;
+import edunova.jp24.model.Smjer;
+import java.math.BigDecimal;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author jalep
+ */
+public class TestObradaSmjer {
+    
+    private ObradaSmjer obrada;
+    
+    public TestObradaSmjer(){
+    obrada = new ObradaSmjer();
+    pripremiPodatke();
+    }
+
+    private void pripremiPodatke() {
+        Smjer s = new Smjer();
+        s.setNaziv("Java programiranje");
+        s.setCijena(new BigDecimal(5000));
+        s.setTrajanje(130);
+        s.setCertifikat(Boolean.TRUE);
+        obrada.setEntitet(s);
+    }
+    
+    
+    @Test
+    public void kreiraj(){
+        try {
+            Smjer s = obrada.create();
+            assertTrue("Objekt nije pohranjen u bazu", !s.getId().equals(Long.valueOf(0)));
+        } catch (Exception e) {
+        }
+}
+    
+
+}
