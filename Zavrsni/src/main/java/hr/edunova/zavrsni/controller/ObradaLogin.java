418a1a5579137fe2a524e7c54c6d00058e054a43
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-10-12 16:50:24 +0100

    login i obrada

---
+++ Zavrsni/src/main/java/hr/edunova/zavrsni/controller/ObradaLogin.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.zavrsni.controller;
+
+import hr.edunova.zavrsni.model.Login;
+import hr.edunova.zavrsni.util.EdunovaException;
+import java.util.List;
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ *
+ * @author jalep
+ */
+public class ObradaLogin extends ObradaOsoba<Login>{
+
+    @Override
+    public List<Login> read() {
+        return session.createQuery("from Login").list();
+    }
+
+    public Login autoriziraj(String email, String lozinka){
+        
+        
+        Login log = (Login)session.createQuery("from Login l where l.email=:email")
+                .setParameter("email", email).getSingleResult();
+        
+        if(log==null){
+            return null;
+        }
+        
+        
+        return BCrypt.checkpw(lozinka, log.getLozinka()) ? log : null;
+    }
+    
+    @Override
+    protected void kontrolaCreate() throws EdunovaException {
+        
+    }
+
+    @Override
+    protected void kontrolaUpdate() throws EdunovaException {
+        
+    }
+
+    @Override
+    protected void kontrolaDelete() throws EdunovaException {
+        
+    }
+    
+}
---
+++ Zavrsni/src/main/java/hr/edunova/zavrsni/model/Login.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.zavrsni.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jalep
+ */
+@Entity
+public class Login extends Osoba{
+    
+    private String lozinka;
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+    
+    
+    
+}
