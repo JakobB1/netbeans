6509267aa03949750f457e4ffb6fbf296a3058bb
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-10-13 11:58:04 +0100

    create

---
+++ Zavrsni/src/main/java/hr/edunova/zavrsni/view/ProzorSucelje.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.zavrsni.view;
+
+/**
+ *
+ * @author jalep
+ */
+public interface ProzorSucelje {
+    
+    public void postaviVrijednostiUEntitet();
+    public void postavke();
+    public void ucitaj();
+    
+}
