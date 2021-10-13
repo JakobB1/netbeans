7997aef05c894914df7248c1f1046396af709cea
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-10-13 13:52:10 +0100

    hash

--- Zavrsni/src/main/java/hr/edunova/zavrsni/model/Operater.java
+++ Zavrsni/src/main/java/hr/edunova/zavrsni/model/Operater.java
@@ -7,6 +7,7 @@ package hr.edunova.zavrsni.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
@@ -83,6 +84,51 @@ public class Operater extends Entitet{
     @Override
     public String toString() {
         return naziv;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.naziv);
+        hash = 59 * hash + Objects.hashCode(this.brojUgovora);
+        hash = 59 * hash + Objects.hashCode(this.certifikat);
+        hash = 59 * hash + Objects.hashCode(this.oib);
+        hash = 59 * hash + Objects.hashCode(this.email);
+        hash = 59 * hash + Objects.hashCode(this.racuni);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Operater other = (Operater) obj;
+        if (!Objects.equals(this.naziv, other.naziv)) {
+            return false;
+        }
+        if (!Objects.equals(this.brojUgovora, other.brojUgovora)) {
+            return false;
+        }
+        if (!Objects.equals(this.oib, other.oib)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.certifikat, other.certifikat)) {
+            return false;
+        }
+        if (!Objects.equals(this.racuni, other.racuni)) {
+            return false;
+        }
+        return true;
     }
     
     
