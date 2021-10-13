95eb7d673190ea1928f088e2ca99e292ce034c07
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-10-13 13:12:38 +0100

    dodavanje ProzorRacun

--- Zavrsni/src/main/java/hr/edunova/zavrsni/view/Izbornik.java
+++ Zavrsni/src/main/java/hr/edunova/zavrsni/view/Izbornik.java
@@ -72,6 +72,11 @@ public class Izbornik extends javax.swing.JFrame {
         jMenu1.add(jMenuItem3);
 
         jMenuItem4.setText("Racuni");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem4);
         jMenu1.add(jSeparator1);
 
@@ -117,6 +122,10 @@ public class Izbornik extends javax.swing.JFrame {
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         new ProzorProizvod().setVisible(true);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        new ProzorRacun().setVisible(true);
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     
 
