5aa150315f18a871caa0afd9dc64d310dd1fafd1
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-10-20 12:46:39 +0100

    Obrada grupe - dodavanje clana

--- polaznikjp24/src/main/java/edunova/jp24/controller/ObradaGrupa.java
+++ polaznikjp24/src/main/java/edunova/jp24/controller/ObradaGrupa.java
@@ -5,6 +5,7 @@
  */
 package edunova.jp24.controller;
 
+import edunova.jp24.model.Clan;
 import edunova.jp24.model.Grupa;
 import edunova.jp24.util.EdunovaException;
 import java.util.List;
@@ -18,6 +19,34 @@ public class ObradaGrupa extends Obrada<Grupa>{
     @Override
     public List<Grupa> read() {
         return session.createQuery("from Grupa").list();
+    }
+    
+    public Grupa create() throws EdunovaException{
+ 
+          for(Clan c : entitet.getPolaznici()){
+              c.setGrupa(entitet);
+            session.save(c);
+        }
+        
+       return super.create();
+    }
+    
+    public Grupa update() throws EdunovaException{
+        session.beginTransaction();
+        for(Clan c : entitet.getPolaznici()){
+            session.save(c);
+        }
+        session.getTransaction().commit();
+        return super.update();
+    }
+    
+     public void delete() throws EdunovaException{
+         session.beginTransaction();
+         for(Clan c : entitet.getPolaznici()){
+            session.delete(c);
+        }
+         session.getTransaction().commit();
+        super.delete();
     }
 
     @Override
@@ -34,7 +63,7 @@ public class ObradaGrupa extends Obrada<Grupa>{
 
     @Override
     protected void kontrolaDelete() throws EdunovaException {
-        if(entitet.getPolaznici().size()>0){
+        if(entitet.getPolaznici()!=null && entitet.getPolaznici().size()>0){
             throw new EdunovaException("Ne možete obrisati grupu koja ima polaznike");
         }
     }
