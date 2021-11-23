82f1cc3f8ef204fc899e42505759a939e9fa8349
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-11-23 14:32:49 +0100

    Entity

--- ZavrsniEng/src/main/java/hr/edunova/zavrsnieng/model/Entity.java
+++ ZavrsniEng/src/main/java/hr/edunova/zavrsnieng/model/Entity.java
@@ -1,4 +1,4 @@
-/*
+  /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -27,7 +27,5 @@ public abstract class Entity {
 
     public void setId(Long id) {
         this.id = id;
-    }
-    
-        
+    }    
 }
