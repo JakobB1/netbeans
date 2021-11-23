1f8b93f67e09e6ca15a739eb31a2f5e904c26220
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-11-23 14:59:59 +0100

    Operator

---
+++ ZavrsniEng/src/main/java/hr/edunova/zavrsnieng/model/Operator.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.zavrsnieng.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+/**
+ *
+ * @author jalep
+ */
+@Entity
+public class Operator extends Entitet{
+    
+    private String name;
+    private String contractNumber;
+    private Boolean certificate;
+    private String oib;
+    private String email;
+    
+    @OneToMany(mappedBy = "operater")
+    private List<Receipt> receipts = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+
+    public Boolean getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(Boolean certificate) {
+        this.certificate = certificate;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Receipt> getReceipts() {
+        return receipts;
+    }
+
+    public void setReceipts(List<Receipt> receipts) {
+        this.receipts = receipts;
+    }  
+}
