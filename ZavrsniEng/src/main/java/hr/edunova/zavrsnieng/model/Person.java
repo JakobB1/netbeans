050cb36b580c96531109a9b9fc4db81869802eab
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-11-23 14:40:04 +0100

    Person

---
+++ ZavrsniEng/src/main/java/hr/edunova/zavrsnieng/model/Person.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.zavrsnieng.model;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author jalep
+ */
+@MappedSuperclass
+public abstract class Person extends Entity{
+    
+    private String name;
+    private String surname;
+    
+    @Column(columnDefinition = "char(11)")
+    private String oib;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }   
+}
